1dddca0a6767315e64fdfbc3c6c2899a1b31496f
tasmto <mtokotashinga@gmail.com>
2024-03-25 18:58:20 +0200

    change port

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(
   PORT,
